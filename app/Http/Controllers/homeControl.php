169717241fc9f5154b8c5497d41267b8e3a99d64
Deva <ndeva45@yahoo.com>
2021-12-12 22:01:04 +0800

    first commit

---
+++ app/Http/Controllers/homeControl.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class homeControl extends Controller
+{
+    public function index()
+    {
+        return view("home");
+    }
+}
